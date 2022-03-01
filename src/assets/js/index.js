2bd2947950d7d8a22689dac49a4984ef4e0725cb
edsumaneev <edsumaneev@yandex.ru>
2022-03-01 21:49:29 +0300

    2022-03-01

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -85,3 +85,22 @@ if ($(".intro").length > 0) {
 };
 
 // 
+// productcard__counter 
+if ($('.goods').length > 0) {
+  $(function () {
+    $('.goods__decrease').click(function () {
+      var $input = $(this).parent().find('input');
+      var count = parseInt($input.val()) - 1;
+      count = count < 1 ? 1 : count;
+      $input.val(count);
+      $input.change();
+      return false;
+    });
+    $('.goods__increase').click(function () {
+      var $input = $(this).parent().find('input');
+      $input.val(parseInt($input.val()) + 1);
+      $input.change();
+      return false;
+    });
+  });
+}
